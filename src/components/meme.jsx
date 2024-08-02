236f74a37e814971e9c660413cd47901a4c4ee08
taf-s <tafara.sithole@gmail.com>
2024-08-02 12:52:33 +0200

    added jsdoc and comments to the meme component

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,32 +1,50 @@
 import React from "react"
 
+/**
+ * Renders a meme component that displays a random meme image and allows the user to input top and bottom text.
+ *
+ * @return {JSX.Element} The rendered meme component.
+ */
 function Meme() {
+    // State to hold information about the meme
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
+    // State to hold the list of all memes
     const [allMemes, setAllMemes] = React.useState([])
 
-    
+    // Fetch the list of memes from the Imgflip API and update the state
     React.useEffect(() => {
-        async function getMemes() {
+        async function fetchMemes() {
             const res = await fetch("https://api.imgflip.com/get_memes")
             const data = await res.json()
             setAllMemes(data.data.memes)
         }
-        getMemes()
+        fetchMemes()
     }, [])
-    
+
+    /**
+     * Generates a random meme image and updates the state with the selected image.
+     *
+     * @return {void}
+     */
     function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
+        const randomIndex = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomIndex].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
     }
-    
+
+    /**
+     * Updates the state of the `meme` object with the new value from the input field.
+     *
+     * @param {Event} event - The event object triggered by the input field.
+     * @return {void} This function does not return anything.
+     */
     function handleChange(event) {
         const {name, value} = event.target
         setMeme(prevMeme => ({
@@ -34,10 +52,11 @@ function Meme() {
             [name]: value
         }))
     }
-    
+
     return (
         <main>
             <div className="form">
+                {/* Top text input */}
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -46,6 +65,7 @@ function Meme() {
                     value={meme.topText}
                     onChange={handleChange}
                 />
+                {/* Bottom text input */}
                 <input 
                     type="text"
                     placeholder="Bottom text"
@@ -54,6 +74,7 @@ function Meme() {
                     value={meme.bottomText}
                     onChange={handleChange}
                 />
+                {/* Button to get a new meme image */}
                 <button 
                     className="form--button"
                     onClick={getMemeImage}
@@ -61,6 +82,7 @@ function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
+            {/* Display the meme image with the top and bottom text */}
             <div className="meme">
                 <img src={meme.randomImage} className="meme--image" />
                 <h2 className="meme--text top">{meme.topText}</h2>
